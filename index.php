fe0da7ad245e55cb4d44cddb362140c924514883
TapSidee <132420599+TapSidee@users.noreply.github.com>
2024-05-20 11:32:55 +0400

    Add files via upload

--- index.php
+++ index.php
@@ -20,6 +20,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LibrarySidee</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- <link rel="stylesheet" href="assets/css/bootstrap.min.css"> -->
     <link rel="stylesheet" href="assets/css/styles.css">
     <style>
         .footer {
